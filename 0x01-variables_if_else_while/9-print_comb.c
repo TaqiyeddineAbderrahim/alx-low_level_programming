fba5b9c9876cd28c217744e5f12f516a0cdf2768
Taqiyeddine Abderrahim <taquieddine.abderrahim@gmail.com>
2023-09-08 05:06:39 -0700

    Round 37: Testing task 09

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - entry point.
+ *
+ * Description: Explaining the task.
+ * Prints all possible combinations of single-digit numbers.
+ *
+ * Return: 0 (Just a random value to stop the program)
+ */
+int main(void)
+{
+	/* Declare and initialize the string variable lower_alphabet */
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		putchar(48 + i);	/* 0-9 range in the ASCII Table */
+		if (i == 9)
+			break;
+		putchar(', ');
+	}
+	putchar('\n');
+
+	return (0);
+}
