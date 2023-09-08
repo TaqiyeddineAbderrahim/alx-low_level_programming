d390d90f2089e0d470b57155120b35c67ddc2e3e
Taqiyeddine Abderrahim <taquieddine.abderrahim@gmail.com>
2023-09-08 02:17:56 -0700

    Round 06: Testing task 02

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -18,7 +18,7 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-      	l_digit = n % 10;
+	l_digit = n % 10;
 
 	if (l_digit > 5)
 		printf("Last digit of %d is %d and is greater than 5\n", n, l_digit);
