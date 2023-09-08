1a68f37b5afb5b629ad43683475cf36ba5fd4161
Taqiyeddine Abderrahim <taquieddine.abderrahim@gmail.com>
2023-09-08 02:06:35 -0700

    Round 02: Testing tasks 0 to 02

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -16,11 +16,11 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
 
 	if (n > 0)
-		printf("%d is positive\n", n)
+		printf("%d is positive\n", n);
 	else if (n == 0)
-		printf("%d is zero\n")
+		printf("%d is zero\n");
 	else
-		printf("%d is negative\v")
+		printf("%d is negative\v");
 
 	return (0);
 }
