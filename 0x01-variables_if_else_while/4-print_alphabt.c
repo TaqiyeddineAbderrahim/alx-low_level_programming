433b387b55d7781e83074688d0d062c9016c767a
Taqiyeddine Abderrahim <taquieddine.abderrahim@gmail.com>
2023-09-08 03:16:50 -0700

    Round 17: Testing task 04

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - entry point.
+ *
+ * Description: Explaining the task.
+ * This function prints the alphabet in lowercase except e and q,
+ * and we can use putchar() maximum two times.
+ *
+ * Return: 0 (Just a random value to stop the program)
+ */
+int main(void)
+{
+	/* Declare and initialize the string variable low_al */
+	char low_al[] = "abcdefghijklmnopqrstuvwxyz";
+	int i;
+
+	for (i = 0; i < 26; i++)
+		if ((low_al[i] != 'e') && (low_al != 'q'))
+			putchar(low_al[i]);
+	putchar('\n');
+
+	return (0);
+}
