7b219c6169a2a23d1f28fe28cfdce0184b73e43e
Taqiyeddine Abderrahim <taquieddine.abderrahim@gmail.com>
2023-09-08 03:23:13 -0700

    Round 19: Testing task 05

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - entry point.
+ *
+ * Description: Explaining the task.
+ * Prints all single digit numbers of base 10 starting from 0,
+ * followed by a new line.
+ *
+ * Return: 0 (Just a random value to stop the program)
+ */
+int main(void)
+{
+	/* Declare and initialize the string variable lower_alphabet */
+	int i;
+
+	for (i = 0; i < 10; i++)
+		putchar((char)i);
+	putchar('\n');
+
+	return (0);
+}i
