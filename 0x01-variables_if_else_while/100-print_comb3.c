a9417dab82039c1081b449340c137240bc93ddc3
Taqiyeddine Abderrahim <taquieddine.abderrahim@gmail.com>
2023-09-08 06:43:37 -0700

    Round 43: Testing task 10

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -15,20 +15,19 @@ int main(void)
 
 	for (i = 0; i < 10; i++)
 		for (j = 0; j < 10; j++)
+		{
 			if (i == j)
 				continue;
-			else if (i < j)
+			if (i < j)
 			{
 				putchar('0' + i);
 				putchar('0' + j);
-				if ((i != 8) && (j != 9))
-				{
-					putchar(',');
-					putchar(' ');
-				}
+				if ((i * j) == 72)  /* if i = 8 & j = 9 */
+					break;
+				putchar(',');
+				putchar(' ');
 			}
-
+		}
 	putchar('\n');
-
 	return (0);
 }
