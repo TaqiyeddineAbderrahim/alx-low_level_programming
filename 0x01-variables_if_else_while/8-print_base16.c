42d98186ab4401e76270ea22b765826e5b655ac4
Taqiyeddine Abderrahim <taquieddine.abderrahim@gmail.com>
2023-09-08 04:58:03 -0700

    Round 35: Testing task 08

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - entry point.
+ *
+ * Description: Explaining the task.
+ * Prints all single digit numbers of base 16 starting from 0,
+ * Using only putchar() (x2 maximum), followed by a new line.
+ *
+ * Return: 0 (Just a random value to stop the program)
+ */
+int main(void)
+{
+	/* Declare and initialize the string variable lower_alphabet */
+	int i;
+	char hexEquiv;
+
+	for (i = 0; i < 10; i++)
+		putchar(48 + i);	/* 0-9 range in the ASCII Table */
+
+	for (hexEquiv = 'a'; hexEquiv <= 'f'; hexEquiv++)
+		putchar(hexEquiv)
+
+	putchar('\n');
+
+	return (0);
+}
