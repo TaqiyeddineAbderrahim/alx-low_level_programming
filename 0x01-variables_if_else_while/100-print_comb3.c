0183cf074a05addef4305cda3cfbe3292568f605
Taqiyeddine Abderrahim <taquieddine.abderrahim@gmail.com>
2023-09-08 05:25:23 -0700

    Round 39: Testing task 10

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+/**
+ * main - entry point.
+ *
+ * Description: Explaining the task.
+ * Prints all possible different combinations of two digits.
+ *
+ * Return: 0 (Just a random value to stop the program)
+ */
+int main(void)
+{
+	/* Declare and initialize the variables*/
+	int i, j;
+
+	for (i = 0; i < 10; i++)
+		for (j = 0; j < 10; j++)
+			if (i < j)
+			{
+				putchar('0' + i);
+				putchar('0' + j);
+			}
+			else if (i == j)
+				continue;
+			else
+			{
+				putchar(',');
+				putchar(' ');
+			}
+	putchar('\n');
+
+	return (0);
+}
