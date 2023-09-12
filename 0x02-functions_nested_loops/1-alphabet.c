e29787181665a3e322eb636c028a359857bc9cd2
Taqiyeddine Abderrahim <taquieddine.abderrahim@gmail.com>
2023-09-12 13:26:57 -0700

    Round 04: Testing 01

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * main - Prints the alphabet, in lowercase, followed by an \n.
+ * print_alphabet - Prints the alphabet, in lowercase, followed by an \n.
  */
 void print_alphabet(void)
 {
