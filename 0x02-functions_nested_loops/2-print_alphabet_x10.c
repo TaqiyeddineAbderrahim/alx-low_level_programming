295112bd3535f63c2f38b3d9a6f1c09c07de79ba
Taqiyeddine Abderrahim <taquieddine.abderrahim@gmail.com>
2023-09-12 13:33:21 -0700

    Round 05: Testing 02

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - Prints the alphabet x10, in lowercase,
+ * followed by an \n.
+ */
+void print_alphabet_x10(void)
+{
+	char low_al;
+	int i = 0;
+
+	while (i < 10)
+	{
+		for (low_al = 'a'; low_al <= 'z'; low_al++)
+			_putchar(low_al);
+		_putchar('\n');
+	}
+}
