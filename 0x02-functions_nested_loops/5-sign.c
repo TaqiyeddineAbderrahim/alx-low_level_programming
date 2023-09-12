72e0d2a85b756b76f5156851394f1db56962a4d2
Taqiyeddine Abderrahim <taquieddine.abderrahim@gmail.com>
2023-09-12 14:08:38 -0700

    Round 14: Testing Task 05

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _print_sign -  Prints the sign of a number.
+ * @c: Number to pass to print_sign function.
+ * Return: -1, 0 or 1.
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+	    _putchar('+');
+	    return (1);
+	}
+	else if (n == 0)
+	{
+	    _putchar('0');
+	    return (0);
+	}
+	else
+	{
+	    _putchar('-');
+	    return (-1);
+	}
+}
