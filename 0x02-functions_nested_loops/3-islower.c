62d9c783dccaca146fa9f8400bccaaa9b0ead159
Taqiyeddine Abderrahim <taquieddine.abderrahim@gmail.com>
2023-09-12 13:49:56 -0700

    Round 09: Testing Task 03

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,11 +1,11 @@
 #include "main.h"
 
 /**
- * is_lower - Checks for lowercase character.
+ * _islower - Checks for lowercase character.
  * @c: Character.
  * Return: 0 or 1.
  */
-int is_lower(char c)
+int _islower(char c)
 {
 	if ((c >= 'a') && (c <= 'z'))
 		return 1;
