6640aec53da7229c6cc1a41530f73a62b5a5890b
Taqiyeddine Abderrahim <taquieddine.abderrahim@gmail.com>
2023-09-12 14:00:44 -0700

    Round 12: Testing Task 04

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _isalpha - Checks for alphabetic character.
+ * @c: Number to pass to _isalpha function.
+ * Return: 0 or 1.
+ */
+int _isalpha(int c)
+{
+	if ((c >= 'a') && (c <= 'z')) || ((c >= 'A') && (c <= 'Z'))
+		return (1);
+	return (0);
+}i
