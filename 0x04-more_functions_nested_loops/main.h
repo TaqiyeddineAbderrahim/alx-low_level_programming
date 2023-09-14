061b4cecf6fbb1722d2b8a2d30a36f9b573ab5e0
Taqiyeddine Abderrahim <taquieddine.abderrahim@gmail.com>
2023-09-14 10:05:49 -0700

    Round 03: Testing Task 0

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,6 +1,13 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+/*
+ * File: main.h
+ * Auth: Taqiyeddine Abderrahim
+ * Desc: Header file containing prototypes for all functions
+ *       used in the 0x03-more_functions_nested_loops directory.
+ */
+
 int _putchar(char c);
 int _isupper(int c);
 
