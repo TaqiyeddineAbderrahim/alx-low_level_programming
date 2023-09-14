387df160d5753d527a44b62b5f889d338fa3c0c7
Taqiyeddine Abderrahim <taquieddine.abderrahim@gmail.com>
2023-09-14 11:21:45 -0700

    Round 09: Testing Task 02

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * mul - Multiplies two integers.
+ * @a: An integer to be multiplied.
+ * @b: A second integer to be multiplied.
+ *
+ * Return: An integer
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
