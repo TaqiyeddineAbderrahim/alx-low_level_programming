ebe05e34a4ab54106575550089b45e35780e66ac
Taqiyeddine Abderrahim <taquieddine.abderrahim@gmail.com>
2023-09-14 11:15:18 -0700

    Round 07: Testing Task 01

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -2,11 +2,10 @@
 
 /**
  * _isdigit - Checks for a digit (0 through 9).
- * @c - The character to be checked.
+ * @c: The character to be checked.
  *
  * Return: 0 or 1
  */
-
 int _isdigit(int c)
 {
 	if (c >= '0' && c <= '9')
